8d528c4258cf1fc3f3dbed18f474dd3d4447fc75
Nyior Clement Jr <nyior.clement@aun.edu.ng>
2019-09-28 11:52:36 +0100

    home icon added

--- complain_app/urls.py
+++ complain_app/urls.py
@@ -5,7 +5,7 @@ from .views import (home_view,
                     complainStatus_view,
                     ComplainCreateView,
                     PostListView,
-                    AgencyLoginView
+                    agency_login_view
 )
 
 
@@ -14,7 +14,7 @@ urlpatterns = [
     #path('', home_view, name = 'home'),
     path ('complainStatus/', complainStatus_view, name = 'complain-status'),
     path('complain/', ComplainCreateView.as_view(), name = 'complain-create'),
-    path('agencylogin/', AgencyLoginView, name = 'agency-login'),
+    path('agencylogin/', agency_login_view, name = 'agency-login'),
     path('', PostListView.as_view(), name = 'home')
 
 ]
